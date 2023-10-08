27e76d389681a7b32d904fa896238179a009b845
Jindra Petřík <jindra.petrik@gmail.com>
2023-10-08 17:09:28 +0200

    Test github actions check

---
+++ src/com/jpexs/test/Main.java
@@ -0,0 +1,11 @@
+package com.jpexs.test;
+/**
+ *
+ * @author JPEXS
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+            System.out.println("not correctly indented");
+    }
+}
