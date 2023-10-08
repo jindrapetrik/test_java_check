05529ab54ba689d9aed8352c081981e4b970f9ae
Jindra Petřík <jindra.petrik@gmail.com>
2023-10-08 17:39:54 +0200

    test fix

--- src/com/jpexs/test/Main.java
+++ src/com/jpexs/test/Main.java
@@ -5,8 +5,9 @@ package com.jpexs.test;
  * @author JPEXS
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world");
-            System.out.println("not correctly indented");
+        System.out.println("not correctly indented");
     }
 }
